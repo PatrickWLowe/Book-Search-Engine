cef05def346538eb6cd845d4edc641c31d47d76f
Pat Lowe <lowe-patrick@att.net>
2023-09-02 21:28:48 -0700

    deploying heroku database errors have been fixed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,5 @@
 const mongoose = require("mongoose");
 
-console.log(process.env.MONGODB_URI, "patrick this is thee uri");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
-// mongoose
-//   .connect(
-//     "mongodb+srv://patricklowe1991:wqJBEQ34i30IwXiL@cluster0.vwbtnsb.mongodb.net/googlebooks?retryWrites=true&w=majority",
-//     { useNewUrlParser: true, useUnifiedTopology: true }
-//   )
-//   .catch((err) => console.log(err));
 
 module.exports = mongoose.connection;
